0b640962abcd1eb11a94660d53ca03c2e37e64c1
EC2 Default User <ec2-user@ip-172-31-34-223.us-east-2.compute.internal>
2019-07-31 19:47:26 +0000

    Works well/not prefectly needs more styling

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,14 @@ import os
 from flask import Flask, flash, request, redirect, render_template, session, url_for
 from werkzeug.utils import secure_filename
 from flask_pymongo import PyMongo
+from gridfs import GridFS
+#name of database
+app.config['MONGO_DBNAME'] = 'FintechNotes' 
+
+# URI of database
+app.config['MONGO_URI'] = 'mongodb+srv://Ysbely05:3L4RUXYgdSg844u@cluster0-ykjcg.mongodb.net/FintechNotes?retryWrites=true&w=majority' 
+
+mongo = PyMongo(app)
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
@@ -21,22 +29,21 @@ def upload_file():
     if request.method == 'GET':
         return render_template ('upload.html')
     else:
-        # check if the post request has the file part
         if 'file' not in request.files:
-			flash('No file part')
-			return redirect(request.url)
-		file = request.files['file']
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
         if file.filename == '':
-			flash('No file selected for uploading')
-			return redirect(request.url)
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			flash('File successfully uploaded')
-			return redirect('/')
-		else:
-			flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
-			return redirect(request.url)
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('File successfully uploaded')
+            return redirect('/add')
+        else:
+            flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+            return redirect(request.url)
 
 
 
@@ -44,13 +51,7 @@ def upload_file():
 
 
 
-#name of database
-app.config['MONGO_DBNAME'] = 'FintechNotes' 
 
-# URI of database
-app.config['MONGO_URI'] = 'mongodb+srv://Ysbely05:3L4RUXYgdSg844u@cluster0-ykjcg.mongodb.net/FintechNotes?retryWrites=true&w=majority' 
-
-mongo = PyMongo(app)
 @app.route('/')
 @app.route('/notes')
 def notes():
