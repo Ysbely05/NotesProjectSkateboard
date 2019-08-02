6a073765e5485ecadca241e166f885fa4281f797
EC2 Default User <ec2-user@ip-172-31-34-223.us-east-2.compute.internal>
2019-08-02 13:44:09 +0000

    Notes added to some of our code

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ UPLOAD_FOLDER = 'uploads'
 app = Flask(__name__)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# Gives a max size od 16 MB per images/file
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 from app import routes
