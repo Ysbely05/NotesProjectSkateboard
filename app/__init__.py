8784ad086629e9287ed4cb2b829b64a72edc6c84
EC2 Default User <ec2-user@ip-172-31-34-223.us-east-2.compute.internal>
2019-08-04 04:57:46 +0000

    Fixed a simple error

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ UPLOAD_FOLDER = 'uploads'
 
 app = Flask(__name__)
 app.secret_key = "secret key"
+# add the files to the "upload" folder
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # Gives a max size od 16 MB per images/file
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
